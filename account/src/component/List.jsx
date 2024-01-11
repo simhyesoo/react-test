08bb3e1956ee77b4b82c03b8066b5668da23975e
simhyesoo <140944587+simhyesoo@users.noreply.github.com>
2024-01-12 01:35:12 +0900

    Add files via upload

---
+++ account/src/component/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function List(props) {
+    return (
+        <div className='list'>
+            <ul>
+                <li>
+                    <p>1.17</p>
+                    <p>강남역 커피빈</p>
+                    <p>-5,200</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default List;

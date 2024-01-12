97e1b7dab02dfa70d75db89828aeb11c00c0088e
simhyesoo <sgptn00@gmail.com>
2024-01-12 09:50:09 +0900

    Reapply "Add files via upload"
    This reverts commit ac24031343940f04b8a8d2a0dc82667ee47c29ab.

---
+++ account/src/component/Total.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Total(props) {
+    return (
+        <div className='total'>
+            <button>+</button>
+            <p>2024년 입출금 내역</p>
+            <p>35,000,000</p>
+        </div>
+    );
+}
+
+export default Total;
